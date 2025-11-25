0c30fd1ded78e8d3964a11022fddcb3b74cd2bd2
Eden Mor <xepooooop@gmail.com>
2025-11-25 13:57:16 +0200

    WIP Language implementation

--- IMS/Program.cs
+++ IMS/Program.cs
@@ -1,5 +1,8 @@
 using IMS.Components;
 using IMS.Helpers;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +15,36 @@ builder.Services.AddHttpClient<InventoryApiClient>(client =>
     client.BaseAddress = new Uri("https://localhost:7084");
 });
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+var supportedCultures = new[]
+{
+    new CultureInfo("en-US"),
+    new CultureInfo("he-IL")
+};
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+    options.RequestCultureProviders =
+    [
+        new CookieRequestCultureProvider()
+    ];
+});
+
 var app = builder.Build();
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en-US"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
+
+var locOptions = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(locOptions.Value);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -30,5 +62,28 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapGet("/setlanguage/{culture}", (string culture, HttpContext context) =>
+{
+    var supportedCultures = new[] { "en-US", "he-IL" };
+    if (!supportedCultures.Contains(culture))
+        culture = "en-US"; // default fallback
+
+    var cookieOptions = new CookieOptions
+    {
+        Expires = DateTimeOffset.UtcNow.AddYears(1),
+        IsEssential = true,
+        Secure = true,
+        SameSite = SameSiteMode.Lax
+    };
+
+    context.Response.Cookies.Append(
+        CookieRequestCultureProvider.DefaultCookieName,
+        CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+        cookieOptions
+    );
+
+    return Results.Redirect("/");
+});
 
 app.Run();
